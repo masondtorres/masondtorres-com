3572a007da27b76ffc9db9913784543aed60f91f
masondtorres <masondanieltorres@gmail.com>
2026-08-04 15:56:41 -0400

    Simplify primary navigation

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -7,9 +7,6 @@ export function SiteHeader() {
         <Link className="brand" href="/">Mason Torres</Link>
         <nav aria-label="Primary navigation">
           <Link href="/books">Books</Link>
-          <Link href="/series">Series</Link>
-          <Link href="/topics">Topics</Link>
-          <Link href="/start-here">Start Here</Link>
           <Link href="/about">About</Link>
         </nav>
       </div>
