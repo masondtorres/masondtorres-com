c87d989251529f24de19e1f2ff6c09e9fd81f03b
masondtorres <masondanieltorres@gmail.com>
2026-09-15 16:26:53 -0400

    Audit 2: align footer, about next steps, and Smokies product name

--- components/SiteFooter.js
+++ components/SiteFooter.js
@@ -12,7 +12,7 @@ export function SiteFooter() {
           <Link href="/books">Books</Link>
           <Link href="/projects">Projects</Link>
           <Link href="/websites">Websites</Link>
-          <Link href="/resources">Catalog & Free Resources</Link>
+          <Link href="/resources">Resources</Link>
           <Link href="/about">About</Link>
           <Link href="/privacy">Privacy</Link>
         </nav>
